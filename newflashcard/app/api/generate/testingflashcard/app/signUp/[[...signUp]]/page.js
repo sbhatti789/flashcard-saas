70819744a3b91d06cce2ca269432d3528fa6a947
Haroon Hakimi <haroonhakimi2005@gmail.com>
2024-08-15 18:50:59 -0400

    hopefully this works

---
+++ newflashcard/app/api/generate/testingflashcard/app/signUp/[[...signUp]]/page.js
@@ -0,0 +1,44 @@
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
+import { SignUp } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function SignUpPage() {
+  return (
+    <Container maxWidth="100vw">
+      <AppBar position="static" sx={{ backgroundColor: "#3f51b5" }}>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <Button color="inherit">
+            <Link href="/signUp" passHref>
+              Sign Up
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="/signIn" passHref>
+              Login
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="h4">Sign Up</Typography>
+        <SignUp />
+      </Box>
+    </Container>
+  );
+}
