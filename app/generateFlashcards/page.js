3915c5cddacee44ed1384c5ce10c52d4622d3adf
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-08-15 15:01:25 -0500

    began flashcards generator page, updated env file

---
+++ app/generateFlashcards/page.js
@@ -0,0 +1,120 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import {
+  Container,
+  Box,
+  Typography,
+  Paper,
+  TextField,
+  Button,
+} from "@mui/material";
+import { writeBatch } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Generate() {
+  const { isLoaded, isSignedIn, user } = useUser;
+  const [flashcards, setFlashcards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  // Send notes to our api to generate flashcards
+  const handleSubmit = async () => {
+    fetch("api/generate", {
+      method: "POST",
+      body: text,
+    })
+      .then((res) => res.json())
+      .then((data) => setFlashcards(data));
+  };
+
+  const handleCardClick = (id) => {
+    setFlipped((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const saveFlashcards = async () => {
+    if (!name) {
+      alert("Please enter a name");
+      return;
+    }
+
+    const batch = writeBatch(db); // using batch because we want to everything all at once
+    const userDocRef = doc(collection(db, "users"), user.id);
+    const docSnap = await userDocRef;
+
+    if (docSnap.exists()) {
+      const collections = docSnap.data().flashcards || [];
+      if (collections.find((f) => f.name === name)) {
+        alert("Flashcard collection with the same name already exists.");
+        return;
+      } else {
+        collections.push({ name });
+        batch.set(userDocRef, { flashcards: collections }, { merge: true });
+      }
+    } else {
+      batch.set(userDocRef, { flashcards: [{ name }] });
+    }
+
+    const colRef = collections(userDocRef, name);
+    flashcards.forEach((flashcard) => {
+      const cardDocRef = doc(colRef);
+      batch.set(cardDocRef, flashcard);
+    });
+
+    await batch.commit();
+    handleClose();
+    router.push("/flashcards");
+  };
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          mt: 4,
+          mb: 6,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4">Generate Flashcards</Typography>
+        <Paper sx={{ p: 4, width: "100%" }}>
+          <TextField
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            label="Enter Text"
+            fullWidth
+            multiline
+            rows={4}
+            variant="outlined"
+            sx={{ mb: 2 }}
+          ></TextField>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            fullWidth
+          >
+            {" "}
+            Submit
+          </Button>
+        </Paper>
+      </Box>
+    </Container>
+  );
+}
