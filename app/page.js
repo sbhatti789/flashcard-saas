6ab9c82291e688ac895367948ea48d25f2dfb27f
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-08-16 08:58:31 -0500

    connected homepage to flashcard generator page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -11,30 +11,29 @@ import Head from "next/head";
 
 // Main Home Component
 export default function Home() {
-
-  const handleSubmit = async()=>{
-    const checkoutSession = await fetch('/api/checkout_session', {
-      method: 'POST',
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch("/api/checkout_session", {
+      method: "POST",
       headers: {
-        origin: 'https://localhost:3000',
+        origin: "https://localhost:3000",
       },
-    })
+    });
 
-    const checkoutSessionJson = await checkoutSession.json()
+    const checkoutSessionJson = await checkoutSession.json();
 
     if (checkoutSession.statusCode === 500) {
-      console.error(checkoutSession.message)
-      return
+      console.error(checkoutSession.message);
+      return;
     }
-    const stripe = await getStripe()
-    const{error} = await stripe.redirectToCheckout({
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
       sessionId: checkoutSessionJson.id,
-    })
+    });
 
-    if (error){
-      console.warn(error.message)
+    if (error) {
+      console.warn(error.message);
     }
-  }
+  };
 
   return (
     // Container for the entire page layout
@@ -82,7 +81,12 @@ export default function Home() {
           The easiest way to make flashcards from your text
         </Typography>
         {/* Get Started button */}
-        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+          href="/generateFlashcards"
+        >
           Get Started
         </Button>
       </Box>
@@ -184,7 +188,12 @@ export default function Home() {
                 Unlimited flashcards and storage with priority support.
               </Typography>
               {/* Button to choose the Pro plan */}
-              <Button variant="contained" color="primary" sx={{ mt: 2 }} onClick={handleSubmit}>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                onClick={handleSubmit}
+              >
                 Choose Pro
               </Button>
             </Box>
